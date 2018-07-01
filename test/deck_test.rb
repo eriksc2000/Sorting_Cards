d30dc5f3acdceb488dec44f26dd343a630ed6fcd
Erik Schneider <erikschneider2211@gmail.com>
2018-07-01 11:34:40 -0600

    adds store cards test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,5 +14,14 @@ class DeckTest < Minitest::Test
     
     assert_instance_of Deck, deck 
   end 
+  
+  def test_it_can_store_cards
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    
+    assert_equal [card_1, card_2, card_3], deck.cards 
+  end
 
 end 
