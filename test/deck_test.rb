3fa49369bc82af635467621432462a44a52ec225
Erik Schneider <erikschneider2211@gmail.com>
2018-07-01 11:40:58 -0600

    adds deck count method and test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -23,5 +23,14 @@ class DeckTest < Minitest::Test
     
     assert_equal [card_1, card_2, card_3], deck.cards 
   end
+  
+  def test_it_can_count_number_of_cards
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    
+    assert_equal 3, deck.count 
+  end 
 
 end 
