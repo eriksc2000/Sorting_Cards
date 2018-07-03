6854c7492ab8fc27654c1c09a182478468d42b42
Erik Schneider <erikschneider2211@gmail.com>
2018-07-02 19:39:02 -0600

    adds record guess method and test

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,29 @@
-require './card'
-require './guess'
-require './deck'
+require './lib/card'
+require './lib/guess'
+require './lib/deck'
 
 class Round 
-  attr_reader :deck, :guesses 
+  attr_reader :deck, :guesses, :correct_guess 
   
   def initialize(deck)
     @deck = deck 
     @guesses = []
+    @correct_guess = 0
   end 
   
   def current_card
     deck.cards[0]
   end 
+  
+  def record_guess(response)
+    response = "#{response [:value]} of #{[:suit]}"
+    guess = Guess.new(response, current_card)
+    @guesses << guess 
+    if guess.feedback == "Correct"
+      @correct_guess += 1
+    end 
+    deck.cards.shift
+    guess 
+  end 
+  
 end
