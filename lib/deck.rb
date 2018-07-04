8741b0552f608d1cf3cf47946386268ce4be04b3
Erik Schneider <erikschneider2211@gmail.com>
2018-07-04 17:09:24 -0600

    adds sort method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,18 @@ class Deck
     @cards.count 
   end 
   
+  def sort
+    loop do 
+      swapped = false 
+        (count-1).times do |i|
+        if cards[i].card_total > cards[i + 1].card_total
+          @cards[i], @cards[i+1] = @cards[i + 1], @cards[i]
+          swapped = true 
+        end 
+      end 
+        break if not swapped 
+    end
+    @cards 
+  end 
+  
 end 
--- lib/guess.rb
+++ lib/guess.rb
@@ -23,5 +23,5 @@ class Guess
       "Incorrect."
     end 
   end 
-  
+    
 end
