faa5cdb22f127dfedda6caf7a016bcb6dbf901f7
Erik Schneider <erikschneider2211@gmail.com>
2018-07-04 19:41:38 -0600

    adds deck merge sort method and test but not yet working

--- lib/deck.rb
+++ lib/deck.rb
@@ -26,4 +26,27 @@ class Deck
     @cards 
   end 
   
+  def merge_sort(deck)
+    if deck.length <= 1
+      deck 
+    else 
+      mid = (deck.length / 2).floor
+      left = merge_sort(deck[0..mid - 1])
+      right = merge_sort(deck[mid..deck.length])
+      merge(left, right)
+    end 
+  end 
+  
+  def merge(left,right)
+    if left.empty?
+      right
+    elsif right.empty?
+      left 
+    elsif left.first < right.first 
+      [left.first] + merge(left[1..left.length], right)
+    else 
+      [right.first] + merge(left, right[1..right.length])
+    end 
+  end 
+  
 end 
